27f6e91a2a344ae1c7676487854f7748db209d0d
jerrygoyal <1gouravgg@gmail.com>
2017-11-29 11:36:41 +0530

    save only on close popup

--- js/eventPage.js
+++ js/eventPage.js
@@ -1,20 +1,42 @@
 /* [alias : jerrygoyal] */
 
 "use strict";
+var ta_content_id = "ta_content_id";
 chrome.runtime.onMessage.addListener(callback);
+
+var inFocus = true;  // global boolean to keep track of state
+chrome.windows.onFocusChanged.addListener(function(window) {
+    closeEvent();
+    if (window == chrome.windows.WINDOW_ID_NONE) {
+        inFocus = false;
+    } else {
+        inFocus = true;
+    }
+});
 function callback(obj, sender, sendResponse) {
-    if(obj){
-        if(obj.method=='saveContent'){
-        saveContent(obj.data)
-        }else if(obj.method=='getContent'){
-            let notes = (getContent(sendResponse));
-            
+    if (obj) {
+        if (obj.method == 'saveContent') {
+            saveContent(obj.data)
+        } else if (obj.method == 'getContent') {
+            getContent(sendResponse);
+
         }
     }
     return true;
- }
+}
 
- function getContent(sendResponse) {
+function closeEvent(){
+    let notes;
+    var views = chrome.extension.getViews({
+        type: "popup"
+    });
+    for (var i = 0; i < views.length; i++) {
+        notes = views[i].document.getElementById(ta_content_id).value;
+    }
+    saveContent(notes);
+}
+
+function getContent(sendResponse) {
     chrome.storage.local.get(["jgNotes"], function (obj) {
         var notes = $.trim(obj["jgNotes"]);
         notes = notes ? notes + '\n' : '';
--- js/script_popup.js
+++ js/script_popup.js
@@ -2,27 +2,28 @@
 
 "use strict";
 var ta_content_id = "ta_content_id";
-var ta_contentRef = $("#"+ta_content_id+"");
+var ta_contentRef = $("#" + ta_content_id + "");
 
-function callEventPageMethod(method, data, callback){
-    chrome.runtime.sendMessage({method: method,data:data }, function(response) {
-        callback(response)});
+function callEventPageMethod(method, data, callback) {
+    chrome.runtime.sendMessage({ method: method, data: data }, function (response) {
+        if(typeof callback === "function") callback(response)
+    });
 }
 
 
- function setContent () {
-     callEventPageMethod('getContent','', function(notes){
+function setContent() {
+    callEventPageMethod('getContent', '', function (notes) {
         ta_contentRef.val(notes);
         // save content in sync storage in case of quota exceeded last time.
         saveContent();
         setCursor();
-     });
-   
+    });
+
 }
 // save  notes in local as well as sync storage
-function saveContent () {
+function saveContent() {
     var notes = ta_contentRef.val();
-    callEventPageMethod('saveContent',notes);
+    callEventPageMethod('saveContent', notes);
 }
 //set position of cursor
 function setCursor(position) {
@@ -41,26 +42,26 @@ function setCursor(position) {
 var jgPopupIns;
 var jgPopupClass = function () {
     var self = this;
-    
+
     self.init = function () {
         setPosition();
         setContent();
         setEventListeners();
     };
 
-    var setPosition = function () {}
+    var setPosition = function () { }
 
     var setEventListeners = function () {
         ta_contentRef.on('keyup', function (event) {
             keydownHandler(event);
-        }); 
+        });
         //window.addEventListener('beforeunload', saveContent);
-        ta_contentRef.on('input',function(e){
-            saveContent();
-          });
+        ta_contentRef.on('input', function (e) {
+            //saveContent();
+        });
     }
 
-    
+
 
     // insert tab on tab key press
     var insertTab = function (e) {
@@ -69,7 +70,7 @@ var jgPopupClass = function () {
         var notes = ta_contentRef.val().substring(0, start) + "\t" + ta_contentRef.val().substring(end);
         ta_contentRef.val(notes);
         setCursor(end + 1);
-        saveContent();
+        //saveContent();
     }
     var keydownHandler = function (e) {
         //support for tab key
@@ -78,10 +79,6 @@ var jgPopupClass = function () {
             insertTab(e);
         }
     }
-
-    
-
-    
 };
 
 (function () {
